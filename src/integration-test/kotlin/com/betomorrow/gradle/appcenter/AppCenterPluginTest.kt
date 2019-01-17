8f2cf4a54560c17ca29ea7984ed51fec82d6427f
Olivier Gauthier <ogauthier@betomorrow.com>
2019-01-17 21:19:41 +0100

    change integration test to use full android application

--- src/integration-test/kotlin/com/betomorrow/gradle/appcenter/AppCenterPluginTest.kt
+++ src/integration-test/kotlin/com/betomorrow/gradle/appcenter/AppCenterPluginTest.kt
@@ -1,29 +1,16 @@
 package com.betomorrow.gradle.appcenter
 
-import org.gradle.internal.impldep.org.junit.Rule
-import org.gradle.internal.impldep.org.junit.rules.TemporaryFolder
 import org.gradle.testkit.runner.GradleRunner
 import org.junit.jupiter.api.Test
+import java.io.File
 
 class AppCenterPluginTest {
 
-    @Rule
-    val testProjectDir = TemporaryFolder()
-
     @Test
     fun testApplyPlugin() {
-        testProjectDir.create()
-        val buildFile = testProjectDir.newFile("build.gradle")
-
-        buildFile.appendText("""
-            plugins {
-                id 'com.betomorrow.appcenter'
-            }
-            """.trimIndent()
-        )
 
         val result = GradleRunner.create()
-            .withProjectDir(testProjectDir.root)
+            .withProjectDir(File("src/integration-test/resources/MyApplication"))
             .withArguments("tasks", "--stacktrace", "--all")
             .withPluginClasspath()
             .withDebug(true)
