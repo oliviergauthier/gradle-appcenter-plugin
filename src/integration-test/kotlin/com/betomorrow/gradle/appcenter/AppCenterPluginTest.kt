352ccaf047b32fbc1b4e92662ed24034b7408686
Olivier Gauthier <ogauthier@betomorrow.com>
2019-08-22 07:58:48 +0200

    Change GroupId for local maven deployment

--- src/integration-test/kotlin/com/betomorrow/gradle/appcenter/AppCenterPluginTest.kt
+++ src/integration-test/kotlin/com/betomorrow/gradle/appcenter/AppCenterPluginTest.kt
@@ -10,9 +10,8 @@ class AppCenterPluginTest {
     fun testApplyPlugin() {
 
         val result = GradleRunner.create()
-            .withProjectDir(File("/Users/ogauthier/projets/amelia/td-amelia2-phone"))
-//            .withProjectDir(File("src/integration-test/resources/MyApplication"))
-            .withArguments("clean", "uploadDevRelease", "--stacktrace", "-PownerName=ogauthierbto",  "-PapiToken=dbf35aa0107b408840e3d8d7216ab8f1f7fbd8eb")
+            .withProjectDir(File("src/integration-test/resources/MyApplication"))
+            .withArguments("clean", "uploadRelease", "--stacktrace", "-PownerName=",  "-PapiToken=")
             .withPluginClasspath()
             .withDebug(true)
             .build()
